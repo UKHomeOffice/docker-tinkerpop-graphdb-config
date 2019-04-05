cc2b5f4b44d835d86bbf33fb2f4d88fbd407ff30
Leonardo Martins <lmartins@pontusnetworks.com>
2019-04-05 10:22:05 +0100

    reduced import list from globals.groovy

--- kube/conf/globals.groovy
+++ kube/conf/globals.groovy
@@ -1,17 +1,6 @@
-import com.pontusvision.utils.ElasticSearchHelper
-import groovy.json.JsonOutput
-import groovy.json.JsonSlurper
-import groovy.transform.CompileDynamic
-import groovy.transform.CompileStatic
-import org.apache.commons.math3.util.Pair
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource
-import org.apache.tinkerpop.gremlin.structure.Edge
-import org.apache.tinkerpop.gremlin.structure.Vertex
-import org.janusgraph.core.*
-import org.janusgraph.core.schema.*
-import org.janusgraph.graphdb.types.vertices.JanusGraphSchemaVertex
-
-import java.util.concurrent.atomic.AtomicInteger
+import org.janusgraph.core.JanusGraph
+import org.janusgraph.core.schema.JanusGraphManagement
 
 LinkedHashMap globals = [:]
 globals << [g: ((JanusGraph) graph).traversal() as GraphTraversalSource]
